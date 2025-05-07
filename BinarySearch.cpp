91c98488f37152919c69b8a415359ce23c746194
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-05-07 13:20:35 +0700

    Menambahkan Looping Do...While ke-2

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -77,7 +77,26 @@ void binarySearch()
 
         int low = 0;
         int high = nPanjang - 1;
-    }while (condition);
+
+        do
+       {
+            int mid = (low + high) / 2;
+            if (element[mid] == x)
+           {
+                cout << "\n[x] Elemen " << x << " ditemukan pada indeks " << mid << ".\n";
+       	  return;
+            }
+            if (x < element[mid])
+           {
+                high = mid - 1;
+            }
+           if (x > element[mid])
+           {
+                low = mid + 1;
+            }
+        }while (low > high);
+
+    }while (ulang == 'y' || ulang == 'Y');
     
 
 }
