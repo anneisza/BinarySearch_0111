2640d5119bf450347accf26f4c9cbd32368eb4cb
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-05-07 13:12:43 +0700

    Membuat Prosedur Display

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -47,3 +47,10 @@ void bubbleSortArray()
         pass++;
      } while (pass <= nPanjang - 1);
 }
+
+void display()
+{
+    cout << "n=======================================\n";
+    cout << "  Element Array Setelah Diurutkan (Asc) \n";
+    cout << "========================================\n";
+}
